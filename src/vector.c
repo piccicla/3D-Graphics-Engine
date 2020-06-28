475277a5ad4fd07fc1a65161c19b40637be5e92a
piccicla <picci2001@yahoo.it>
2020-06-28 11:20:14 +0100

    update270620

--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,88 @@
 #include <math.h>
 #include "vector.h"
 
+
+// vector magnitude
+float vec2_length(vec2_t v){
+	return sqrt(v.x * v.x + v.y * v.y);
+}
+float vec3_length(vec3_t v){
+	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+//vector addition/subtraction/multiplication/division
+vec2_t vec2_add(vec2_t a, vec2_t b){
+	vec2_t result =	{ 	.x = a.x + b.x ,
+						.y = a.y + b.y 
+					};
+	return result;
+}
+vec3_t vec3_add(vec3_t a, vec3_t b){
+	vec3_t result = { 	.x = a.x + b.x,
+						.y = a.y + b.y, 
+						.z = a.z + b.z  
+					};
+	return result;	
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b){
+	vec2_t result = { 	.x = a.x - b.x,
+						.y = a.y - b.y 
+					};
+	return result;	
+}
+vec3_t vec3_sub(vec3_t a, vec3_t b){
+	vec3_t result = { 	.x = a.x - b.x,
+						.y = a.y - b.y,
+						.z = a.z - b.z
+					};
+	return result;	
+}
+
+vec2_t vec2_mul(vec2_t a, float scale){
+	vec2_t result =	{	.x = a.x * scale,
+						.y = a.y * scale
+					};
+	return result;
+}
+vec3_t vec3_mul(vec3_t a, float scale){
+	vec3_t result =	{	.x = a.x * scale,
+						.y = a.y * scale,
+						.z = a.z * scale
+					};
+	return result;	
+}
+vec2_t vec2_div(vec2_t a, float scale){
+	vec2_t result =	{	.x = a.x / scale,
+						.y = a.y / scale
+					};
+	return result;
+}
+vec3_t vec3_div(vec3_t a, float scale){
+	vec3_t result =	{	.x = a.x / scale,
+						.y = a.y / scale,
+						.z = a.z / scale
+					};
+	return result;	
+}
+
+// vector crossproduct
+vec3_t vec3_cross(vec3_t a, vec3_t b){
+	vec3_t result = { 	.x = (a.y * b.z) - (a.z * b.y),
+						.y = (a.z * b.x) - (a.x * b.z), 
+						.z = (a.x * b.y) - (a.y * b.x)  
+					};
+	return result;	
+}
+// float dot product
+float vec2_dot(vec2_t a, vec2_t b){
+	return (a.x * b.x) + (a.y * b.y);
+}
+float vec3_dot(vec3_t a, vec3_t b){
+	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+// rotate vectors
 vec3_t vec3_rotate_x(vec3_t v, float angle){
 	vec3_t rotated_vector = {
 		.x = v.x,
--- src/vector.h
+++ src/vector.h
@@ -12,8 +12,32 @@ typedef struct {
 	float y;
 }  vec2_t;
 
+
+//vector magnitude
+float vec2_length(vec2_t v);
+float vec3_length(vec3_t v);
+
+//vector addition/subtraction/multiplication/division
+vec2_t vec2_add(vec2_t a, vec2_t b);
+vec3_t vec3_add(vec3_t a, vec3_t b);
+vec2_t vec2_sub(vec2_t a, vec2_t b);
+vec3_t vec3_sub(vec3_t a, vec3_t b);
+vec2_t vec2_mul(vec2_t a, float scale);
+vec3_t vec3_mul(vec3_t a, float scale);
+vec2_t vec2_div(vec2_t a, float scale);
+vec3_t vec3_div(vec3_t a, float scale);
+
+// vector cross product (perpendicolar vector)
+vec3_t vec3_cross(vec3_t a, vec3_t b);
+// float dot product (0 when 90 degrees)
+float vec2_dot(vec2_t a, vec2_t b);
+float vec3_dot(vec3_t a, vec3_t b);
+
+//rotate vectors
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
+
+
 
 #endif
